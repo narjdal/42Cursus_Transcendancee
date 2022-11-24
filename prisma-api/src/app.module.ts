fa662eb390853dbef7d6cd46e32ff1c35beec940
Mohamed fadel Labrayji <mlabrayj@e2r8p11.1337.ma>
2022-11-24 10:44:20 +0100

    mainbranch

--- prisma-api/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PlayerModule } from './player/player.module';
-import { AuthModule } from './auth/auth.module';
-
-@Module({
-  imports: [PlayerModule, AuthModule],
-})
-export class AppModule {}
