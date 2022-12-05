75855f8c10fd49d23bd0426c89dbded23bfdb07b
emy-sg <imane1sg@gmail.com>
2022-12-05 13:47:18 +0100

    without nodes modules

--- prisma-api/dist/auth/auth.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Response } from 'express';
-import { AuthService } from './auth.service';
-export declare class AuthController {
-    private readonly authService;
-    constructor(authService: AuthService);
-    authentication(): Promise<void>;
-    login(request: any, res: Response): Promise<void>;
-    logout(request: any, res: Response): Promise<void>;
-}
