e9fa1d11864a283d7114f6314980f1d8ed5f2efd
root <root@DESKTOP-NL4P4AJ.localdomain>
2022-12-08 16:27:14 +0100

    Handle Online users . [Event.Gateway file]

--- prisma-api/src/chat/chat.module.ts
+++ prisma-api/src/chat/chat.module.ts
@@ -3,8 +3,9 @@ import { MessagesService } from './chat.service';
 import {  ChatGateway } from './chat.gateway';
 import { PrismaService } from 'src/prisma.service';
 import { PlayerService } from 'src/player/player.service';
+import { EventsGateway } from './events.gateway';
 
 @Module({
-  providers: [ChatGateway, MessagesService, PrismaService, PlayerService],
+  providers: [ChatGateway, MessagesService, PrismaService, PlayerService, EventsGateway],
 })
 export class MessagesModule {}
